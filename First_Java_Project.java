e0cddeb446fbcd1158bcd659cca794f9ad3156ba
KingTayte <tatenda.charlesm@gmail.com>
2022-03-15 08:47:55 +0200

    Create First_Java_Project.java

---
+++ First_Java_Project.java
@@ -0,0 +1,14 @@
+// My First Hello World Java Projects //
+
+public class Application{
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+}
+
+
+/* By Tatenda Mapfumo aka KingTayte
+
+15/03/2022 08:30am
+
+*/
